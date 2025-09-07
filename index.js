d63661407a0f8ea87f4814f53f435f4cae69fe21
maria <ritukhan534@gmail.com>
2025-09-07 19:48:07 +0600

    up

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`✅ Task Manager API running at http://localhost:${PORT}`);
+  console.log(` Task Manager API running at http://localhost:${PORT}`);
 });
